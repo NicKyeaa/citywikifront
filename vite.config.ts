6a07ab9817916f0faca1c8b93e43423f4184fbd3
Tin <tin.jurcevic@gmail.com>
2023-08-20 21:29:55 +0200

    Changed vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/geonames-all-cities-with-a-population-1000.json": {
-        target:
-          "http://127.0.0.1:8080/geonames-all-cities-with-a-population-1000.json", // Your API server address
+        target: "http://127.0.0.1:8080", // Your API server address
         changeOrigin: true,
       },
     },
