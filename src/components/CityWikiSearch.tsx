86b3577ac0f62b70368850b6fd3a3b5c01137416
Tin <tin.jurcevic@gmail.com>
2023-08-20 20:40:59 +0200

    Added generatecity function

--- src/components/CityWikiSearch.tsx
+++ src/components/CityWikiSearch.tsx
@@ -1,10 +1,28 @@
 import { Space, Button } from "antd";
 
 const CityWikiSearch: React.FC = () => {
+  const generateCity = async () => {
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:8080/geonames-all-cities-with-a-population-1000.json"
+      );
+      if (!response.ok) {
+        throw new Error("Response not ok");
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fatching data:", error);
+    }
+  };
+
   return (
     <>
       <Space direction='vertical'>
-        <Button type='primary'>Primary Button</Button>
+        <Button type='primary' onClick={generateCity}>
+          Primary Button
+        </Button>
       </Space>
     </>
   );
